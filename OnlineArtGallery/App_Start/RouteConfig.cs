881692ab5e69db1d7351f8ae8ba0840a900cb334
Nhan <nhan.nguyenkieu317@gmail.com>
2023-06-02 21:16:51 +0700

    commit

--- OnlineArtGallery/App_Start/RouteConfig.cs
+++ OnlineArtGallery/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace OnlineArtGallery
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "FEHome", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ OnlineArtGallery/Controllers/FEHomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineArtGallery.Controllers
+{
+    public class FEHomeController : Controller
+    {
+        // GET: FEHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
