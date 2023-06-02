f4305a05920ebd5708e37140f73a61a01d619aed
Nhan <nhan.nguyenkieu317@gmail.com>
2023-06-02 21:04:17 +0700

    Add project files.

---
+++ OnlineArtGallery/Controllers/BEIndexController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineArtGallery.Controllers
+{
+    public class BEIndexController : Controller
+    {
+        // GET: BEIndex
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
