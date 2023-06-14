2e4f690bf9a0def79f16923f0f67f0a6b9a933a9
Nhan <nhan.nguyenkieu317@gmail.com>
2023-06-14 21:34:32 +0700

    commit

---
+++ OnlineArtGallery/Controllers/BEAuctionController.cs
@@ -0,0 +1,34 @@
+﻿using OnlineArtGallery.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineArtGallery.Controllers
+{
+    public class BEAuctionController : Controller
+    {
+        private GalleryArtEntities db = new GalleryArtEntities();
+        // GET: BEAuction
+        [HttpPost]
+        public string Create(Auction obj)
+        {
+            var item = new Auction();
+            item.artwork_id = obj.artwork_id;
+            item.auction_reserve_price = obj.auction_reserve_price;
+            item.auction_start_date = obj.auction_start_date;
+            item.auction_end_date = obj.auction_end_date;
+            item.auction_created_date = DateTime.Now.ToString("yyyy-MM-dd");
+            db.Auctions.Add(item);
+            db.SaveChanges();
+
+            var artwork = db.Artworks.Find(obj.artwork_id);
+            artwork.artwork_status = 2;
+            db.SaveChanges();
+
+            return "Add auction successfully";
+        }
+    }
+}
