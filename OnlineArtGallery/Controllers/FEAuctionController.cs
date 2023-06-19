298ed0e7bd8e48c1898a820e005bed777e03ebf3
Retsh <retsh@ditityze.aisa>
2023-06-19 14:53:00 +0700

    commit

---
+++ OnlineArtGallery/Controllers/FEAuctionController.cs
@@ -0,0 +1,40 @@
+﻿using OnlineArtGallery.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineArtGallery.Controllers
+{
+    public class FEAuctionController : Controller
+    {
+        private GalleryArtEntities db = new GalleryArtEntities();
+        // GET: FEAuction
+        public ActionResult Create(string auction_amount, int id)
+        {
+            if (Session["UserId"] == null)
+                return Json("Login first!");
+
+            var auc = new Auction_User()
+            {
+                auction_id = id,
+                user_id = int.Parse(Session["UserId"].ToString()),
+                auction_user_created_date = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss"),
+                auction_amount = auction_amount
+            };
+            db.Auction_User.Add(auc);
+
+            var auction = db.Auctions.Find(id);
+            auction.auction_current_bid = auction_amount;
+            db.SaveChanges();
+
+            var result = new
+            {
+                noti = "Bid successfully !",
+                bid = auction_amount
+            };
+            return Json(result);
+        }
+    }
+}
