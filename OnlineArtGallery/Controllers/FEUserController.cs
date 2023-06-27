e3e1660f794562df0499f4a7109ec402173b5010
HANHTRAN2710 <103866643+HANHTRAN2710@users.noreply.github.com>
2023-06-27 17:20:15 +0700

    update password user

--- OnlineArtGallery/Controllers/FEUserController.cs
+++ OnlineArtGallery/Controllers/FEUserController.cs
@@ -7,6 +7,9 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.UI.WebControls.WebParts;
 using OnlineArtGallery.Models.ModelView;
+using System.Security.Cryptography;
+using System.Text;
+
 namespace OnlineArtGallery.Controllers
 {
     public class FEUserController : Controller
@@ -139,18 +142,27 @@ namespace OnlineArtGallery.Controllers
         {
             var auth = db.Users.Find(Session["UserId"]);
 
-            if (current_password == null)
-                return "Enter your password first !";
+            
 
             if (new_password != reset_password)
-                return "Your password doesen't match !";
+                return "Your passwords don't match!";
 
-            auth.user_password = new_password;
-            db.SaveChanges();
-
-            return "Change password successfully !";
+            // Kiểm tra khớp với mật khẩu hiện tại
+            if (current_password == auth.user_password && new_password == reset_password)
+            {
+                // Lưu mật khẩu mới vào cơ sở dữ liệu
+                auth.user_password = new_password;
+                db.SaveChanges();
+                return "Password changed successfully!";
+            }
+            else
+            {
+                return "Password changed Fail!";
+            }
         }
 
+        // Hàm băm mật khẩu
+       
         public ActionResult RequestArtwork(Artwork model, string height, string width, HttpPostedFileBase artwork_image)
         {
             var artwork = new Artwork()
